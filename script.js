804b49ef15aab5b6c3370cb6baf1cdff9f8131f2
Kadnys07 <kadnysm|@gmail.com>
2026-02-14 15:16:31 -0300

    Teste de layout concluido

---
+++ script.js
@@ -0,0 +1,20 @@
+
+function fazerLogin(event) {
+    
+    event.preventDefault();
+
+    
+    let email = document.getElementById("email").value;
+    let senha = document.getElementById("senha").value;
+
+    
+    if (senha.length < 6) {
+        alert("Erro: A senha precisa ter pelo menos 6 caracteres.");
+        return; 
+    }
+
+    alert("Login realizado com sucesso! Bem-vindo(a) " + email);
+    
+    // 5. Redireciona para a página inicial
+    window.location.href = "index.html";
+}
